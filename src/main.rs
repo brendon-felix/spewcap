216942c126591e143c9819b01534e81097ea8a13
Brendon Felix <brendon.felix@hp.com>
2025-02-14 16:04:06 -0700

    quick update

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,6 @@
 // TODO: Implement set port command
 //   TODO: Pause spew while setting port
 
-use std::thread;
-// use std::sync::mpsc;
-
 mod settings;
 mod state;
 mod serial;
@@ -17,24 +14,24 @@ mod commands;
 mod utils;
 mod log;
 
-use utils::clear_console;
-use settings::get_settings;
-use state::{init_state, shared_state};
-
 fn main() {
-    clear_console();
-    let settings = get_settings();
-    let state = init_state(settings.timestamps);
-    // let (tx, rx) = mpsc::channel();
-    let shared_state1 = shared_state(&state);
-    let serial_thread = thread::spawn(move || {
+    utils::clear_console();
+    let settings = settings::get_settings();
+    let state = state::init_state(settings.timestamps);
+
+    // Spawn thread for serial connection
+    let shared_state1 = state::shared_state(&state);
+    let serial_thread = std::thread::spawn(move || {
         serial::connect_loop(settings, shared_state1);
     });
-    let shared_state2 = shared_state(&state);
-    let command_thread = thread::spawn(move || {
+
+    // Spawn thread for command handler
+    let shared_state2 = state::shared_state(&state);
+    let command_thread = std::thread::spawn(move || {
         commands::command_loop(shared_state2);
     });
-    // rx.recv()?;
+
     let _ = serial_thread.join().map_err(|e| println!("Serial thread panicked: {:?}", e));
     let _ = command_thread.join().map_err(|e| println!("Command thread panicked: {:?}", e));
+    println!("YAY!")
 }
